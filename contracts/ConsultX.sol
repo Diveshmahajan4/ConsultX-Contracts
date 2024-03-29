c244f66ad5ef0165f358b628870171e23ca7be12
Manas Dalvi <dalvimanas33@gmail.com>
2024-03-29 20:59:11 +0530

    Hardhat configuration

--- contracts/ConsultX.sol
+++ contracts/ConsultX.sol
@@ -1,6 +1,6 @@
 //SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.24;
 
 contract consultVerse {
     uint256 public NoOfRequests = 0;
---
+++ hardhat.config.ts
@@ -0,0 +1,34 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+require("dotenv").config();
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.24",
+  networks: {
+    lumiotestnet: {
+      url: "https://testnet.lumio.io",
+      accounts: [process.env.WALLET_KEY as string],
+      chainId: 9990,
+      gasPrice: 3000000000,
+    },
+    cardano: {
+      url: "https://rpc.cardona.zkevm-rpc.com",
+      chainId: 2442,
+      accounts: [process.env.WALLET_KEY as string],
+    },
+    avail: {
+      url: "https://op-avail-sepolia.alt.technology",
+      chainId: 202402021700,
+      accounts: [process.env.WALLET_KEY as string],
+    },
+    coredao: {
+      url: "https://rpc.test.btcs.network",
+      chainId: 1115,
+      accounts: [process.env.WALLET_KEY as string],
+    },
+  },
+  defaultNetwork: "hardhat",
+};
+
+export default config;
